dab17fe98ce29ba0469609f3878b3ce84d9a57da
gowthamv18 <56287005+gowthamv18@users.noreply.github.com>
2020-03-22 14:11:10 +0800

    Create DataProfiling.py

---
+++ DataProfiling.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+df = pd.read_excel(r"C:\Users\gowtham\Desktop\Staff_data.xlsx")
+profile = ProfileReport(df, title='Pandas Profiling Report', html={'style':{'full_width':True}})
+profile.to_file(r"C:\Users\gowtham\Desktop\test.html")
