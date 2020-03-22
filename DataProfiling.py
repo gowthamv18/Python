7544974b914a663eea024f033ee8174b75d626dc
gowthamv18 <iEOL12345>
2020-03-22 14:55:43 +0800

    new commit

--- DataProfiling.py
+++ DataProfiling.py
@@ -5,3 +5,4 @@ from pandas_profiling import ProfileReport
 df = pd.read_excel(r"C:\Users\gowtham\Desktop\Staff_data.xlsx")
 profile = ProfileReport(df, title='Pandas Profiling Report', html={'style':{'full_width':True}})
 profile.to_file(r"C:\Users\gowtham\Desktop\test.html")
+print(profile)
